40cbc7519c8605eed2a781478e719b039c6780c2
acast872 <140276833+acast872@users.noreply.github.com>
2025-02-04 21:13:47 +0000

    Lab 5 changes

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class Input
+{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Input yout Name : ");
+        String userName = scanner.next();
+        System.out.print("Hello "+ userName + ", What is your age : ");
+        int userAge = scanner.nextInt();
+        System.out.print("What is your weight : ");
+        float userWeight = scanner.nextFloat();
+        System.out.print("Do you smoke? (true/false) : ");
+        boolean userSmokes = scanner.nextBoolean();
+        System.out.println("Hello " + userName + ", your age is " + userAge + ", you weigh " + userWeight + ", and you smoke: " + userSmokes);
+
+    }
+}
